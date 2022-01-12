6c675f1edf9629c7cbef8f279c3ccf12d6d8b2e9
Chase Kessler <chasekessler30@yahoo.com>
2022-01-12 14:18:25 -0500

    update

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableWithoutFeedback, Image } from 'react-native';
 
 export default function App() {
+ 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    // View => UIView IOS
+    <SafeAreaView style={styles.container}>
+      <Text>Lets get this money!!</Text>
+      <TouchableWithoutFeedback>
+        {/* <Image source={{
+          width: 200,
+          height: 300,
+          uri: ""
+        }}>
+
+        </Image> */}
+
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
